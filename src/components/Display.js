f4ac9115c6efd87318e09d0f1299ae8bf52f02bb
Maria Serditova <mariaserditova@mail.ru>
2023-06-16 22:38:43 +0300

    no color

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,27 +1,27 @@
 import React, { useContext } from "react";
 import { NumberContext } from "./NumberProvider";
 import styles from "./styles.module.css";
-import useState from "react";
+// import useState from "react";
 // import EqualButton from "./EqualButton";
 
 const Display = () => {
   const { number, storedNumber, functionType } = useContext(NumberContext);
   const { doMath } = useContext(NumberContext);
-  const [theme, setTheme] = useState(true);
+  // const [theme, setTheme] = useState(true);
 
-  const handleClick = () => {
-    setTheme(!theme);
-    doMath();
-  };
+  // const handleClick = () => {
+  //   setTheme(!theme);
+  //   doMath();
+  // };theme ? styles.red : styles.green
 
   return (
     <div>
-      <p className={theme ? styles.red : styles.green}>
+      <p className={styles.green}>
         {!storedNumber
           ? `${number}`
           : `${storedNumber} ${functionType} ${number}`}
       </p>
-      <button className="white-button" type="button" onClick={handleClick}>
+      <button className="white-button" type="button" onClick={() => doMath()}>
         =
       </button>
     </div>
