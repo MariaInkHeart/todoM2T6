f08c67cc8b47439a93f8ced208569168bb042eb9
Maria Serditova <mariaserditova@mail.ru>
2023-06-21 10:28:32 +0300

    победа дружбы

--- src/components/Game.jsx
+++ src/components/Game.jsx
@@ -1,12 +1,13 @@
 import React, {useState} from 'react';
 import Board from './Board';
 import { calculateWinner } from '../winner';
+import PropTypes from 'prop-types';
 
 const Game = ()=>{
     const [board, setBoard] = useState(Array(9).fill(null));
     const [xIsNext, setXIsNext]= useState(true);
     const winner = calculateWinner(board);
-
+    
     const handleClick= (index)=> {
         const boardCopy =[...board];
         if (winner || boardCopy[index]) return
@@ -15,6 +16,9 @@ const Game = ()=>{
         setXIsNext(!xIsNext)
     }
 
+handleClick.propTypes ={
+        index: PropTypes.number
+    }
     const startNewGame = () => {
         return (
           <button
@@ -25,10 +29,20 @@ const Game = ()=>{
           </button>
         );
       };
+    
+    const totalFinal = () =>{
+        if(winner){
+            return <p> Победа  {winner} </p>
+        } else if (winner === "дружбы"){
+           return <p>Ничья</p>
+        } else{
+           return <p>Ход: {(xIsNext? 'X': 'O')}</p>
+        }
+    }
 
     return(
         <div className='wrapper'>
-            <p className='gameInfo'>{winner? 'Победа:' + winner : 'Ход:'+ (xIsNext? 'X': 'O')}</p>
+            <p className='gameInfo'>{totalFinal()}</p>
 <Board squares={board} click={handleClick}/>
 {startNewGame()}
         </div>
--- src/winner.js
+++ src/winner.js
@@ -11,8 +11,14 @@ export function calculateWinner(squares) {
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
+
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
+    } else if (
+      !squares.includes(null) &&
+      !(squares[a] && squares[a] === squares[b] && squares[a] === squares[c])
+    ) {
+      return "дружбы";
     }
   }
   return null;
